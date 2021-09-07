26238b77499a234dbabf4ece6ea64978ba899b25
ewertonSoares <ewerton.fa4@gmail.com>
2021-09-07 19:38:45 -0300

    Insert no novo banco de dados

---
+++ BD/insert_pacientes.sql
@@ -0,0 +1,4 @@
+INSERT INTO pacientes(idUsuario, cpf, endereco)
+VALUES (1, '111.111.111-11','Rua Florinda Maria, 52, Campinas-SP'),
+(2, '222.222.222-22','Rua peregrino silta, 33, Campinas-SP'),
+(3, '333.333.333-33','Av. Benjamin Constant, 255, Campinas-SP');
